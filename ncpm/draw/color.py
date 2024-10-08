67427e50c902568848e4146936e0f4618b4183fb
Alex Cannan <alexfcannan@gmail.com>
2024-10-07 22:27:19 -0400

    algorithmic color gen using chance to pick distant colors

--- ncpm/draw/color.py
+++ ncpm/draw/color.py
@@ -1,4 +1,5 @@
 import itertools
+import sys
 
 import matplotlib.cm as cm
 import numpy as np
@@ -24,6 +25,20 @@ def color_generator(type: str="discrete"):
             yield ImageColor.getrgb(f"hsl({i}, 50%, 50%)")
             yield ImageColor.getrgb(f"hsl({i+85}, 50%, 50%)")
             yield ImageColor.getrgb(f"hsl({i+171}, 50%, 50%)")
+    elif type == "rainbow_distant":
+        # randomly select hue value. chance to skip a hue value if it's too close to a previous one.
+        # normal distribution with std dev of 64/len(hues) makes closer colors very unlikely at the beginning, becoming more lenient over time
+        hues: list[float] = []
+        while True:
+            hue = np.random.rand() * 256
+            probabilities = [np.exp(-((hue - h) ** 2) / (2 * (64 / len(hues)) ** 2)) for h in hues]
+            # print(hue, hues, probabilities, file=sys.stderr)
+            if not all(np.random.rand() > prob for prob in probabilities):
+                # print(f"hue {hue} failed vibe check", file=sys.stderr)
+                continue
+            else:
+                hues.append(hue)
+                yield ImageColor.getrgb(f"hsl({hue}, 60%, 30%)")
     elif type == "prism":
         for i in itertools.cycle(itertools.chain(np.linspace(0, 1, 100), np.linspace(1, 0, 100))):
             c = tuple([int(128*x) for x in cm.prism(i)])
